86a9e2483906d2f2b2c5d7a7411c864f807bb67c
Edsandro Freitas <edsandrofreitas@gmail.com>
2023-08-27 12:10:38 -0300

    feat: add service to read csv files

---
+++ src/main/java/edsandrof/infnetjavaoo/exceptions/FileException.java
@@ -0,0 +1,8 @@
+package main.java.edsandrof.infnetjavaoo.exceptions;
+
+public class FileException extends RuntimeException {
+
+    public FileException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/edsandrof/infnetjavaoo/service/CsvService.java
@@ -0,0 +1,30 @@
+package main.java.edsandrof.infnetjavaoo.service;
+
+import main.java.edsandrof.infnetjavaoo.exceptions.FileException;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CsvService {
+    public List<String[]> readFile(String path) {
+        List<String[]> fileContent = new LinkedList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+
+            String header = br.readLine(); // discard header
+
+            while ((line = br.readLine()) != null) {
+                fileContent.add(line.trim().split(","));
+            }
+
+        } catch (IOException e) {
+            throw new FileException("Error in file reader: " + e.getMessage());
+        }
+
+        return fileContent;
+    }
+}
