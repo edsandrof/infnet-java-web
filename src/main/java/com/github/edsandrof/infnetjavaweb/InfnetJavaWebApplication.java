f52c912fb55cd3ff01bc5ef58906d0c2285e21a7
Edsandro Freitas <edsandrofreitas@gmail.com>
2023-09-25 20:50:22 -0300

    feat: add spring initializer maven configurations

---
+++ src/main/java/com/github/edsandrof/infnetjavaweb/InfnetJavaWebApplication.java
@@ -0,0 +1,13 @@
+package com.github.edsandrof.infnetjavaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfnetJavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfnetJavaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/edsandrof/infnetjavaweb/InfnetJavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.edsandrof.infnetjavaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfnetJavaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
