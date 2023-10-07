0a5d0c5de6b5d921508ea5cd4a7c363cbe5f158d
Edsandro Freitas <edsandrofreitas@gmail.com>
2023-10-07 15:47:20 -0300

    feat: add vehiclesale controller

---
+++ src/main/java/com/github/edsandrof/infnetjavaweb/application/controller/VehicleSaleController.java
@@ -0,0 +1,46 @@
+package com.github.edsandrof.infnetjavaweb.application.controller;
+
+import com.github.edsandrof.infnetjavaweb.infrastructure.util.Constant.Page;
+import com.github.edsandrof.infnetjavaweb.model.domain.VehicleSale;
+import com.github.edsandrof.infnetjavaweb.model.service.VehicleSaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping(value = "/vehiclesale")
+@Controller
+public class VehicleSaleController {
+
+    private final VehicleSaleService vehicleSaleService;
+
+    @Autowired
+    public VehicleSaleController(VehicleSaleService vehicleSaleService) {
+        this.vehicleSaleService = vehicleSaleService;
+    }
+
+    @GetMapping
+    public String getListPage(Model model) {
+        model.addAttribute("sales", vehicleSaleService.listAll());
+        return Page.VEHICLE_SALE_LIST;
+    }
+
+    @GetMapping(value = "/register")
+    public String getRegisterPage() {
+        return Page.VEHICLE_SALE_REGISTER;
+    }
+
+    @PostMapping(value = "/register")
+    public String register(VehicleSale vehicleSale) {
+        vehicleSaleService.register(vehicleSale);
+
+        return Page.REDIRECT;
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id) {
+        vehicleSaleService.delete(id);
+
+        return Page.REDIRECT + Page.VEHICLE_SALE_LIST;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/vehiclesale/list.jsp
@@ -0,0 +1,43 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="ISO-8859-1">
+    <title>VehicleDealer</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<c:import url="/WEB-INF/jsp/menu.jsp"/>
+
+<div class="container-fluid mt-3">
+    <h3>Listagem de Vendas:</h3>
+
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <td>id</td>
+            <td>Data</td>
+            <td>Comprador</td>
+            <td>Veículos</td>
+            <td>Preço total</td>
+            <td></td>
+        </tr>
+        </thead>
+        <tbody>
+        <c:forEach var="s" items="${sales}">
+            <tr>
+                <td>${s.id}</td>
+                <td>${s.purchaseDate}</td>
+                <td>${s.vehicleBuyer}</td>
+                <td>${s.vehicles}</td>
+                <td>${s.totalCost}</td>
+                <td></td>
+            </tr>
+        </c:forEach>
+        </tbody>
+    </table>
+
+</div>
+</body>
+</html>
