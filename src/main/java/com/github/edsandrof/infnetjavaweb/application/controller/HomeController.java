90d8cad8b5d163b35e059677ebea783f2ecfb845
Edsandro Freitas <edsandrofreitas@gmail.com>
2023-10-07 15:31:47 -0300

    feat: add logout route

--- src/main/java/com/github/edsandrof/infnetjavaweb/application/controller/HomeController.java
+++ src/main/java/com/github/edsandrof/infnetjavaweb/application/controller/HomeController.java
@@ -3,6 +3,9 @@ package com.github.edsandrof.infnetjavaweb.application.controller;
 import com.github.edsandrof.infnetjavaweb.infrastructure.util.Constant.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
@@ -15,5 +18,14 @@ public class HomeController {
 	@GetMapping(value = "/login")
 	public String getLoginPage() {
 		return Page.LOGIN;
+	}
+
+	@GetMapping(value = "/logout")
+	public String logout(HttpSession httpSession, SessionStatus sessionStatus) {
+
+		sessionStatus.setComplete();
+		httpSession.removeAttribute("employee");
+
+		return Page.HOME;
 	}
 }
